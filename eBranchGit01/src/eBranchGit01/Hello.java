adadf7d04648830402b2053b538930c71575e9b4
sist <sist@DESKTOP-7UE7LE3>
2019-08-30 16:25:56 +0900

    hh

--- eBranchGit01/src/eBranchGit01/Hello.java
+++ eBranchGit01/src/eBranchGit01/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
 		
 		
 
