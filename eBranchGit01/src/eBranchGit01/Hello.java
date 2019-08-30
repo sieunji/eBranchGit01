2fad6cdec6680f20b40a97716da0385d2fd64aa5
sist <sist@DESKTOP-7UE7LE3>
2019-08-30 15:46:09 +0900

    hotfix

--- eBranchGit01/src/eBranchGit01/Hello.java
+++ eBranchGit01/src/eBranchGit01/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		
+		
 
 	}
 
